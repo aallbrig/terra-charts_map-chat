5049cd45408b892fecc164c363489328ba65dd87
Andrew Allbright <aallbrig@Andrews-MacBook-Pro.local>
2014-12-18 02:21:33 -0500

    Routes

--- app/routes.php
+++ app/routes.php
@@ -14,3 +14,10 @@ Route::get('/', 'ApplicationController@index');
 Route::get('/about', 'ApplicationController@about');
 Route::get('/instructions', 'ApplicationController@instructions');
 Route::post('/postMessage', 'ApplicationController@postMessage');
+Route::get('/test', function(){
+  $chatCircles = ChatCircle::all();
+  foreach ($chatCircles as $chatCircle) {
+    $chatCircle->messages;
+  }
+  return $chatCircles;
+});
